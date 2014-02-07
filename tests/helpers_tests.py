388af25b50fec7e6bc5122be84fa1e30f330cd78
if1live <libsora25@gmail.com>
2014-02-07 23:53:03 +0900

    커버리지 올림

--- tests/helpers_tests.py
+++ tests/helpers_tests.py
@@ -8,3 +8,17 @@ class get_content_path_Test(unittest.TestCase):
     def test_run(self):
         filepath = helpers.get_content_path('sample.md')
         self.assertEqual(True, os.path.isfile(filepath))
+
+class read_content_Test(unittest.TestCase):
+    def test_run(self):
+        filepath = helpers.get_content_path('testdata.md')
+        self.assertEqual(True, os.path.isfile(filepath))
+
+        actual = helpers.read_content(filepath)
+        expected = 'key: test-data\nfor test'
+        self.assertEqual(expected, actual)
+
+class create_document_Test(unittest.TestCase):
+    def test_run(self):
+        helpers.create_document('testdata.md')
+
---
+++ wiki/testdata.md
@@ -0,0 +1,2 @@
+key: test-data
+for test
